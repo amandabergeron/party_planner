941aa653735f3b0bea590c4cee977b43da017690
Amanda Bergeron <bergeron@goldmail.etsu.edu>
2012-05-23 10:06:27 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyPlanner::Application.config.secret_token = '177bc87239b3a38fbd5b6a29beca590663b09d3d734db10c9bfd02ded037af5808600021e66940fca6c15dc1462640abba61b0ca47f5cda40305c621fcc799bb'
